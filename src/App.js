faca449c769c658336bce1cf8e099832d4c5cefd
Mj-Saini <manojsaini97285@gmail.com>
2024-08-27 16:11:04 +0530

    tyre slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import Header from './components/Common/Header';
-import Tyres from './components/Common/Tyres';
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Header from "./components/Common/Header";
+import Tyres from "./components/Common/Tyres";
+
 function App() {
   return (
-    <div>
-      <Header/>
+    <div className="overflow-clip pb-10">
+      <Header />
       <Tyres />
     </div>
   );
--- src/components/Common/Helper.jsx
+++ src/components/Common/Helper.jsx
@@ -1,53 +1,112 @@
 export const dropdownData = [
-    {
-      title: "MOVIES",
-      subTitle: [
-        {
-          title: "NEW Release",
-          link: "category",
-        },
-        {
-          title: "Trending",
-          link: "category",
-        },
-        {
-          title: "Top 50",
-          link: "category",
-        },
-      ],
-    },
-    {
-      title: "SHOWS",
-      subTitle: [
-        {
-          title: "NEW Release",
-          link: "category",
-        },
-        {
-          title: "Trending",
-          link: "category",
-        },
-        {
-          title: "Top 50",
-          link: "category",
-        },
-      ],
-    },
-    // {
-    //   title: "MY ACCOUNT",
-    //   subTitle: [
-    //     {
-    //       title: "NEW Release",
-    //       link: "",
-    //     },
-    //     {
-    //       title: "Trending",
-    //       link: "trending",
-    //     },
-    //     {
-    //       title: "Top 50",
-    //       link: "",
-    //     },
-    //   ],
-    // },
-  ];
+  {
+    title: "MOVIES",
+    subTitle: [
+      {
+        title: "NEW Release",
+        link: "category",
+      },
+      {
+        title: "Trending",
+        link: "category",
+      },
+      {
+        title: "Top 50",
+        link: "category",
+      },
+    ],
+  },
+  {
+    title: "SHOWS",
+    subTitle: [
+      {
+        title: "NEW Release",
+        link: "category",
+      },
+      {
+        title: "Trending",
+        link: "category",
+      },
+      {
+        title: "Top 50",
+        link: "category",
+      },
+    ],
+  },
+  // {
+  //   title: "MY ACCOUNT",
+  //   subTitle: [
+  //     {
+  //       title: "NEW Release",
+  //       link: "",
+  //     },
+  //     {
+  //       title: "Trending",
+  //       link: "trending",
+  //     },
+  //     {
+  //       title: "Top 50",
+  //       link: "",
+  //     },
+  //   ],
+  // },
+];
+
+export const tyresArray = [
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+  {
+    brand: "apollo",
+    prize: "₹ 3,774 ",
+    vName: "145/80 R13",
+    tubeType: "tubeless",
+    warrenty: " 5 Year Warranty",
+  },
+];
